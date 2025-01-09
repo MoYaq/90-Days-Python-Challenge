f1d3973b489bc0df86e3b493d763bbbb1daefc59
MoYaq <legendmohammed3485@gmail.com>
2025-01-09 16:09:19 +0000

    Update Day 4.py

--- Day 4.py
+++ Day 4.py
@@ -5,13 +5,12 @@
 #1.Using a while loop for the countdown
 print("Countdown using a while loop:")
 
-count = 10  # Initialize the starting number
-while count > 0:  # Loop until count is greater than 0
+count = 10  #Initialize the starting number
+while count > 0:  #Loop until count is greater than 0
     print(count)  #Print the current count
     count -= 1  #Decrease count by 1
     
-print("Hurray For Loop Countdown Complete") #Message after while loop countdown
-
+print("Hurray While Loop Countdown Complete") #Message after while loop countdown
 
 #2. Using a for loop for the countdown
 print("Countdown using a for loop:")
