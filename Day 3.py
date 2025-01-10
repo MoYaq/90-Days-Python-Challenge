776b1143a50fa8c27f8a331661a33d4725a5357c
MoYaq <legendmohammed3485@gmail.com>
2025-01-10 12:59:57 +0000

    Update Day 3.py

--- Day 3.py
+++ Day 3.py
@@ -1,7 +1,6 @@
 #Day Three: Conditional Statements
 #Project: Building a simple "age-eligibility/qualification checker"
-#A program to ask for users' ages and determine their qualifications for a session of Moyaq CodersHub bootcamp.
-#Things to learn: Using if, elif, and else statements.
+#A program to demonstrate the using if, elif, and else statements.
 
 #1.a) Ask the user for their name
 name = input("Please, what is your name? ")
